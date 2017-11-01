d48ac43619b4d108e72ba7e9018734301e4f9760
Ubuntu <ubuntu@ip-172-30-0-136.ec2.internal>
2017-11-01 23:58:08 +0000

    this is what giving up looks like

--- application.py
+++ application.py
@@ -108,7 +108,7 @@ def oauth2callback():
   flask.session['credentials'] = credentials_to_dict(credentials)
   save_creds(flask.session['credentials'])
 
-  return flask.redirect('stuff')
+  return ('You are now authorized! Go <a href="/">home</a> now!')
 
 
 @application.route('/stuff')
